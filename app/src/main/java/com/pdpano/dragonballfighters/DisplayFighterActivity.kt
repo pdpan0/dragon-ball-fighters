d89a6a5d1bc21392039dd8a06cf801708f5c4e92
Lucas Martins <lm.m4rtins@gmail.com>
2020-09-07 12:31:27 -0300

    Adicionando imagens na tela de DisplayFighterActivity.kt

--- app/src/main/java/com/pdpano/dragonballfighters/DisplayFighterActivity.kt
+++ app/src/main/java/com/pdpano/dragonballfighters/DisplayFighterActivity.kt
@@ -1,9 +1,12 @@
 package com.pdpano.dragonballfighters
 
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class DisplayFighterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,14 +14,15 @@ class DisplayFighterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_display_fighter)
 
         val fighterName: String = intent.getStringExtra(FIGHTER_NAME)
-        val fighterStrength: Int = intent.getIntExtra(FIGHTER_STRENGTH,0)
-        val fighterHealth: Int = intent.getIntExtra(FIGHTER_HEALTH,0)
+        val fighterStrength: Int = intent.getIntExtra(FIGHTER_STRENGTH, 0)
+        val fighterHealth: Int = intent.getIntExtra(FIGHTER_HEALTH, 0)
         val fighterBreed: String = intent.getStringExtra(FIGHTER_BREED)
 
-        if(fighterName.isNullOrEmpty() ||
-           fighterStrength==0 ||
-           fighterHealth==0 ||
-           fighterBreed.isNullOrEmpty()) {
+        if (fighterName.isNullOrEmpty() ||
+            fighterStrength == 0 ||
+            fighterHealth == 0 ||
+            fighterBreed.isNullOrEmpty()
+        ) {
 
             findViewById<TextView>(R.id.label_fighter).apply {
                 setTextColor(Color.parseColor("#d63031"))
@@ -36,15 +40,28 @@ class DisplayFighterActivity : AppCompatActivity() {
         }
     }
 
-    fun displayFighter(fighter: Fighter) {
-        findViewById<TextView>(R.id.label_fighter).apply {
-            text = if (fighter.getKi() >= 80.0) {
+    private fun displayFighter(fighter: Fighter) {
+        if (fighter.getKi() >= 80.0) {
+            findViewById<TextView>(R.id.label_fighter).apply {
                 setTextColor(Color.parseColor("#0984e3"))
-                "Lutador de alto nível melhor tomar cuidado. \n\n $fighter"
-            } else {
-                setTextColor(Color.parseColor("#d63031"))
-                "Lutador ainda precisa de treinamento. \n\n $fighter"
+                text = "Lutador de alto nível melhor tomar cuidado. \n\n $fighter"
             }
+            setImage("@drawable/instinto")
+        } else {
+            findViewById<TextView>(R.id.label_fighter).apply {
+                setTextColor(Color.parseColor("#d63031"))
+                text = "Lutador ainda precisa de treinamento. \n\n $fighter"
+            }
+            setImage("@drawable/kid")
+        }
+    }
+
+    private fun setImage(imagePath: String) {
+        val imageResource: Int = resources.getIdentifier(imagePath, null, packageName)
+        val image: Drawable? = ContextCompat.getDrawable(applicationContext, imageResource)
+
+        findViewById<ImageView>(R.id.image_fighter).apply {
+            setImageDrawable(image)
         }
     }
 }
